4a4fb7b6a652834789507a080329282ee13df56f
rahulap-aot <rahul.ap@aot-technologies.com>
2024-11-18 02:28:23 -0800

    resposive

---
+++ script.js
@@ -0,0 +1,45 @@
+
+// toggle icon navbar
+
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+
+
+
+// Scroll Sections
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () =>{
+
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        }
+    });
+
+    // Add sticky class to header when scrolling past the top 100 pixels
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+
+    // Remove toggle icon and navbar when scrolling past the top 100 pixels
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+
+}
